4d3344384dca69931d286f1881e586ecc6306de9
Thomas Heartman <thomas@getunleash.io>
2025-11-08 19:17:08 +0100

    initial: add latex and gitignore

---
+++ Commentary (latex)/zotero-postscript.js
@@ -0,0 +1,34 @@
+// Note: this file is not used directly by Zotero. Editing it in place has no
+// effect. However, because Zotero's / BetterBibTeX's postscript entry is awful,
+// it's easier to edit it in a code editor. To have changes take effect, add
+// them to Zotero -> Settings -> BetterBibTeX -> Postscript
+
+if (Translator.BetterBibTeX || Translator.BetterBibLaTeX) {
+  if (zotero.itemType === "audioRecording") {
+    for (const creator of zotero.creators) {
+      if (creator.creatorType === "performer") creator.creatorType = "author";
+    }
+    tex.addCreators();
+
+    if (tex.has.album && !tex.has.booktitle) {
+      tex.add({ name: "booktitle", value: tex.has.album.value });
+    }
+
+    if (zotero.url.includes("soundcloud") && !tex.has.booktitle) {
+      tex.add({ name: "booktitle", value: "Artist upload", enc: "verbatim" });
+    }
+
+    if (tex.has.medium && !tex.has.howpublished) {
+      tex.add({ name: "howpublished", value: tex.has.medium.value });
+    }
+
+    if (!tex.has.howpublished && zotero.url) {
+      if (zotero.url.includes("soundcloud")) {
+        tex.add({ name: "howpublished", value: "Soundcloud" });
+      }
+      if (zotero.url.includes("spotify")) {
+        tex.add({ name: "howpublished", value: "Spotify" });
+      }
+    }
+  }
+}
